3aaf1b8112144044e50c1afd7d90b13a39e537c2
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2018-12-14 09:48:54 -0600

    added first final

---
+++ Hudzik20(FirstFinal)/Hudzik20(FirstFinal)/frmGame.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSelect1 = New System.Windows.Forms.Button()
+        Me.btnSelect2 = New System.Windows.Forms.Button()
+        Me.btnSelect3 = New System.Windows.Forms.Button()
+        Me.btnSelect6 = New System.Windows.Forms.Button()
+        Me.btnSelect5 = New System.Windows.Forms.Button()
+        Me.btnSelect4 = New System.Windows.Forms.Button()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.picDie1 = New System.Windows.Forms.PictureBox()
+        Me.picDie2 = New System.Windows.Forms.PictureBox()
+        Me.picDie3 = New System.Windows.Forms.PictureBox()
+        Me.picDie4 = New System.Windows.Forms.PictureBox()
+        Me.picDie5 = New System.Windows.Forms.PictureBox()
+        Me.picDie6 = New System.Windows.Forms.PictureBox()
+        CType(Me.picDie1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDie2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDie3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDie4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDie5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDie6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSelect1
+        '
+        Me.btnSelect1.Location = New System.Drawing.Point(18, 254)
+        Me.btnSelect1.Name = "btnSelect1"
+        Me.btnSelect1.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect1.TabIndex = 0
+        Me.btnSelect1.Text = "Select First"
+        Me.btnSelect1.UseVisualStyleBackColor = True
+        '
+        'btnSelect2
+        '
+        Me.btnSelect2.Location = New System.Drawing.Point(113, 254)
+        Me.btnSelect2.Name = "btnSelect2"
+        Me.btnSelect2.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect2.TabIndex = 1
+        Me.btnSelect2.Text = "Select Second"
+        Me.btnSelect2.UseVisualStyleBackColor = True
+        '
+        'btnSelect3
+        '
+        Me.btnSelect3.Location = New System.Drawing.Point(212, 254)
+        Me.btnSelect3.Name = "btnSelect3"
+        Me.btnSelect3.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect3.TabIndex = 2
+        Me.btnSelect3.Text = "Select Third"
+        Me.btnSelect3.UseVisualStyleBackColor = True
+        '
+        'btnSelect6
+        '
+        Me.btnSelect6.Location = New System.Drawing.Point(212, 283)
+        Me.btnSelect6.Name = "btnSelect6"
+        Me.btnSelect6.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect6.TabIndex = 5
+        Me.btnSelect6.Text = "Select Sixth"
+        Me.btnSelect6.UseVisualStyleBackColor = True
+        '
+        'btnSelect5
+        '
+        Me.btnSelect5.Location = New System.Drawing.Point(113, 283)
+        Me.btnSelect5.Name = "btnSelect5"
+        Me.btnSelect5.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect5.TabIndex = 4
+        Me.btnSelect5.Text = "Select Fifth"
+        Me.btnSelect5.UseVisualStyleBackColor = True
+        '
+        'btnSelect4
+        '
+        Me.btnSelect4.Location = New System.Drawing.Point(18, 283)
+        Me.btnSelect4.Name = "btnSelect4"
+        Me.btnSelect4.Size = New System.Drawing.Size(89, 23)
+        Me.btnSelect4.TabIndex = 3
+        Me.btnSelect4.Text = "Select Fourth"
+        Me.btnSelect4.UseVisualStyleBackColor = True
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Location = New System.Drawing.Point(18, 225)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirm.TabIndex = 6
+        Me.btnConfirm.Text = "Confirm"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'picDie1
+        '
+        Me.picDie1.Location = New System.Drawing.Point(27, 31)
+        Me.picDie1.Name = "picDie1"
+        Me.picDie1.Size = New System.Drawing.Size(49, 46)
+        Me.picDie1.TabIndex = 7
+        Me.picDie1.TabStop = False
+        '
+        'picDie2
+        '
+        Me.picDie2.Location = New System.Drawing.Point(27, 83)
+        Me.picDie2.Name = "picDie2"
+        Me.picDie2.Size = New System.Drawing.Size(49, 46)
+        Me.picDie2.TabIndex = 8
+        Me.picDie2.TabStop = False
+        '
+        'picDie3
+        '
+        Me.picDie3.Location = New System.Drawing.Point(27, 135)
+        Me.picDie3.Name = "picDie3"
+        Me.picDie3.Size = New System.Drawing.Size(49, 46)
+        Me.picDie3.TabIndex = 9
+        Me.picDie3.TabStop = False
+        '
+        'picDie4
+        '
+        Me.picDie4.Location = New System.Drawing.Point(113, 31)
+        Me.picDie4.Name = "picDie4"
+        Me.picDie4.Size = New System.Drawing.Size(49, 46)
+        Me.picDie4.TabIndex = 10
+        Me.picDie4.TabStop = False
+        '
+        'picDie5
+        '
+        Me.picDie5.Location = New System.Drawing.Point(113, 83)
+        Me.picDie5.Name = "picDie5"
+        Me.picDie5.Size = New System.Drawing.Size(49, 46)
+        Me.picDie5.TabIndex = 11
+        Me.picDie5.TabStop = False
+        '
+        'picDie6
+        '
+        Me.picDie6.Location = New System.Drawing.Point(113, 135)
+        Me.picDie6.Name = "picDie6"
+        Me.picDie6.Size = New System.Drawing.Size(49, 46)
+        Me.picDie6.TabIndex = 12
+        Me.picDie6.TabStop = False
+        '
+        'frmGame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 312)
+        Me.Controls.Add(Me.picDie6)
+        Me.Controls.Add(Me.picDie5)
+        Me.Controls.Add(Me.picDie4)
+        Me.Controls.Add(Me.picDie3)
+        Me.Controls.Add(Me.picDie2)
+        Me.Controls.Add(Me.picDie1)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.btnSelect6)
+        Me.Controls.Add(Me.btnSelect5)
+        Me.Controls.Add(Me.btnSelect4)
+        Me.Controls.Add(Me.btnSelect3)
+        Me.Controls.Add(Me.btnSelect2)
+        Me.Controls.Add(Me.btnSelect1)
+        Me.Name = "frmGame"
+        Me.Text = "frmMainGame"
+        CType(Me.picDie1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDie2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDie3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDie4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDie5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDie6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSelect1 As Button
+    Friend WithEvents btnSelect2 As Button
+    Friend WithEvents btnSelect3 As Button
+    Friend WithEvents btnSelect6 As Button
+    Friend WithEvents btnSelect5 As Button
+    Friend WithEvents btnSelect4 As Button
+    Friend WithEvents btnConfirm As Button
+    Friend WithEvents picDie1 As PictureBox
+    Friend WithEvents picDie2 As PictureBox
+    Friend WithEvents picDie3 As PictureBox
+    Friend WithEvents picDie4 As PictureBox
+    Friend WithEvents picDie5 As PictureBox
+    Friend WithEvents picDie6 As PictureBox
+End Class
---
+++ Hudzik20(FirstFinal)/Hudzik20(FirstFinal)/frmMainMenu.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(80, 133)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start Game"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Hudzik20_FirstFinal_.My.Resources.Resources.placeholder
+        Me.PictureBox1.Location = New System.Drawing.Point(38, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(178, 89)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'frmMainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(268, 189)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "frmMainMenu"
+        Me.Text = "VBFarkle Main Menu"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
