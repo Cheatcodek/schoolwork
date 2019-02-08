f662a91b2071888d6fdf8a903f278d5609fd2616
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-02-08 09:32:52 -0600

    Added wordguessgame

---
+++ Hudzik22(WordGuessGame)/Hudzik22(WordGuessGame)/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Hudzik22(WordGuessGame)")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Hudzik22(WordGuessGame)")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8a485676-7c25-420b-91e9-9441903ea5ef")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Hudzik22(WordGuessGame)/Hudzik22(WordGuessGame)/frmMain.vb
@@ -0,0 +1,43 @@
+﻿Public Class frmMain
+    Dim strMainString As String
+    Dim intCorrectGuesses As Integer
+    Dim intWrongGuesses As Integer
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strMainString = InputBox("Insert One Word", "Word Guess")
+        strMainString = strMainString.Replace(" ", "")
+        strMainString = strMainString.ToLower
+        lblDisplayword.Text = ""
+        lblLength.Text = lblLength.Text & Val(strMainString.Length)
+
+        For intCounter As Integer = 0 To strMainString.Length - 1
+
+            lblDisplayword.Text = lblDisplayword.Text & "_"
+
+        Next
+
+    End Sub
+
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        Dim bolCorrect As Boolean = False
+
+        For intThrowaway As Integer = 0 To strMainString.Length - 1
+
+
+            If txtGuess.Text = strMainString.Chars(intThrowaway) Or txtGuess.Text = strMainString.ToLower.Chars(intThrowaway) Then
+
+                intCorrectGuesses += 1
+                bolCorrect = True
+            End If
+
+
+        Next
+
+        If bolCorrect Then
+            intWrongGuesses -= 1
+        End If
+
+    End Sub
+
+
+End Class
