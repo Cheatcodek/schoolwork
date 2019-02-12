6dc563a7abaeefaaa1f6753927c4f7bc13acb59d
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-02-12 09:41:36 -0600

    hecc

--- Hudzik22(WordGuessGame)/Hudzik22(WordGuessGame)/frmMain.vb
+++ Hudzik22(WordGuessGame)/Hudzik22(WordGuessGame)/frmMain.vb
@@ -1,6 +1,5 @@
 ﻿Public Class frmMain
     Dim strMainString As String
-    Dim intCorrectGuesses As Integer
     Dim intWrongGuesses As Integer
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -11,7 +10,7 @@
         lblLength.Text = lblLength.Text & Val(strMainString.Length) 'Gives the length of the word
 
         For intCounter As Integer = 0 To strMainString.Length - 1
-
+            'Loop to put "blanks" into label
             lblDisplayword.Text = lblDisplayword.Text & "_"
 
         Next
@@ -20,15 +19,16 @@
 
     Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
         Dim bolCorrect As Boolean = False
+        Static strGuessed As String
 
         For intThrowaway As Integer = 0 To strMainString.Length - 1
-
+            'Use loop to find position and existance of characters
             If txtGuess.Text = strMainString.Chars(intThrowaway) Or txtGuess.Text.ToLower = strMainString.ToLower.Chars(intThrowaway) Then
 
-                intCorrectGuesses += 1
                 bolCorrect = True
                 lblDisplayword.Text = lblDisplayword.Text.Remove(intThrowaway, 1) 'Removes the character to be replaced
                 lblDisplayword.Text = lblDisplayword.Text.Insert(intThrowaway, strMainString.Substring(intThrowaway, 1)) 'Inserts the character from the main string into the label
+                strGuessed = strGuessed & lblDisplayword.Text.Chars(intThrowaway)
             End If
 
         Next
@@ -39,13 +39,13 @@
             MessageBox.Show("That is not in the word!" & vbNewLine & txtGuess.Text & " is not in the word!")
         End If
 
-        If intCorrectGuesses = strMainString.Length Then 'Checks if the correct guesses have gotten to the length of the string
+        If lblDisplayword.Text = strMainString Then 'Checks if the correct guesses have gotten to the length of the string
             MessageBox.Show("You have fully guessed the word!" & vbNewLine & "It was " & strMainString)
             Application.Exit()
         End If
 
         If intWrongGuesses = 8 Then
-            MessageBox.Show("You are out of guesses" & vbNewLine & "Therefor you lose!")
+            MessageBox.Show("You are out of guesses" & vbNewLine & "Therefor you lose!" & vbNewLine & "The Word was " & strMainString)
             Application.Exit() 'Informs the user and then exits if they have failed
         End If
 
