c80909efe83a36f09d07ae6c30ee869533c109c5
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-02-15 09:47:39 -0600

    wordguessgame3

---
+++ Hudzik22(WordGuessGame3)/Hudzik22(WordGuessGame)/frmMain.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblInfoDisplayword = New System.Windows.Forms.Label()
+        Me.lblDisplayword = New System.Windows.Forms.Label()
+        Me.lblInfoWrongguesses = New System.Windows.Forms.Label()
+        Me.lblWrongguesses = New System.Windows.Forms.Label()
+        Me.lblLength = New System.Windows.Forms.Label()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.btnToggle = New System.Windows.Forms.Button()
+        Me.tmrLength = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTimer = New System.Windows.Forms.Label()
+        Me.tmrVictory = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblInfoDisplayword
+        '
+        Me.lblInfoDisplayword.AutoSize = True
+        Me.lblInfoDisplayword.Location = New System.Drawing.Point(60, 87)
+        Me.lblInfoDisplayword.Name = "lblInfoDisplayword"
+        Me.lblInfoDisplayword.Size = New System.Drawing.Size(67, 13)
+        Me.lblInfoDisplayword.TabIndex = 0
+        Me.lblInfoDisplayword.Text = "Your Word : "
+        '
+        'lblDisplayword
+        '
+        Me.lblDisplayword.AutoSize = True
+        Me.lblDisplayword.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDisplayword.Location = New System.Drawing.Point(75, 104)
+        Me.lblDisplayword.Name = "lblDisplayword"
+        Me.lblDisplayword.Size = New System.Drawing.Size(49, 16)
+        Me.lblDisplayword.TabIndex = 1
+        Me.lblDisplayword.Text = "Label1"
+        '
+        'lblInfoWrongguesses
+        '
+        Me.lblInfoWrongguesses.AutoSize = True
+        Me.lblInfoWrongguesses.Location = New System.Drawing.Point(26, 28)
+        Me.lblInfoWrongguesses.Name = "lblInfoWrongguesses"
+        Me.lblInfoWrongguesses.Size = New System.Drawing.Size(92, 13)
+        Me.lblInfoWrongguesses.TabIndex = 2
+        Me.lblInfoWrongguesses.Text = "Wrong Guesses : "
+        '
+        'lblWrongguesses
+        '
+        Me.lblWrongguesses.AutoSize = True
+        Me.lblWrongguesses.Location = New System.Drawing.Point(41, 45)
+        Me.lblWrongguesses.Name = "lblWrongguesses"
+        Me.lblWrongguesses.Size = New System.Drawing.Size(13, 13)
+        Me.lblWrongguesses.TabIndex = 3
+        Me.lblWrongguesses.Text = "0"
+        '
+        'lblLength
+        '
+        Me.lblLength.AutoSize = True
+        Me.lblLength.Location = New System.Drawing.Point(124, 140)
+        Me.lblLength.Name = "lblLength"
+        Me.lblLength.Size = New System.Drawing.Size(49, 13)
+        Me.lblLength.TabIndex = 4
+        Me.lblLength.Text = "Length : "
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(12, 163)
+        Me.txtGuess.MaxLength = 1
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(100, 20)
+        Me.txtGuess.TabIndex = 5
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Location = New System.Drawing.Point(29, 190)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirm.TabIndex = 6
+        Me.btnConfirm.Text = "Confirm"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'btnToggle
+        '
+        Me.btnToggle.Location = New System.Drawing.Point(147, 218)
+        Me.btnToggle.Name = "btnToggle"
+        Me.btnToggle.Size = New System.Drawing.Size(87, 23)
+        Me.btnToggle.TabIndex = 7
+        Me.btnToggle.Text = "Toggle Music"
+        Me.btnToggle.UseVisualStyleBackColor = True
+        '
+        'tmrLength
+        '
+        Me.tmrLength.Enabled = True
+        '
+        'lblTimer
+        '
+        Me.lblTimer.AutoSize = True
+        Me.lblTimer.Location = New System.Drawing.Point(176, 9)
+        Me.lblTimer.Name = "lblTimer"
+        Me.lblTimer.Size = New System.Drawing.Size(39, 13)
+        Me.lblTimer.TabIndex = 8
+        Me.lblTimer.Text = "Time : "
+        '
+        'tmrVictory
+        '
+        Me.tmrVictory.Interval = 500
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnConfirm
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(237, 244)
+        Me.Controls.Add(Me.lblTimer)
+        Me.Controls.Add(Me.btnToggle)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.lblLength)
+        Me.Controls.Add(Me.lblWrongguesses)
+        Me.Controls.Add(Me.lblInfoWrongguesses)
+        Me.Controls.Add(Me.lblDisplayword)
+        Me.Controls.Add(Me.lblInfoDisplayword)
+        Me.Name = "frmMain"
+        Me.Text = "Word Guess"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInfoDisplayword As Label
+    Friend WithEvents lblDisplayword As Label
+    Friend WithEvents lblInfoWrongguesses As Label
+    Friend WithEvents lblWrongguesses As Label
+    Friend WithEvents lblLength As Label
+    Friend WithEvents txtGuess As TextBox
+    Friend WithEvents btnConfirm As Button
+    Friend WithEvents btnToggle As Button
+    Friend WithEvents tmrLength As Timer
+    Friend WithEvents lblTimer As Label
+    Friend WithEvents tmrVictory As Timer
+End Class
---
+++ Hudzik22(WordGuessGame3)/Hudzik22(WordGuessGame)/frmMain.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Media
+Public Class frmMain
+    Dim strMainString As String
+    'Fighting Gold is by CODA, recreated in a band arangement by Musescore
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strMainString = InputBox("Insert One Word", "Word Guess")
+        strMainString = strMainString.Replace(" ", "")
+        strMainString = strMainString.ToLower 'Prepares the string for use
+
+        lblDisplayword.Text = "" 'Removes anything in displayword
+        lblLength.Text = lblLength.Text & Val(strMainString.Length) 'Gives the length of the word
+
+        For intCounter As Integer = 0 To strMainString.Length - 1
+            'Loop to put "blanks" into label
+            lblDisplayword.Text = lblDisplayword.Text & "-"
+        Next
+
+    End Sub
+
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        Dim bolCorrect As Boolean = False
+        Static strGuessed As String
+        Static intWrongGuesses As Integer
+
+        For intThrowaway As Integer = 0 To strMainString.Length - 1
+            'Use loop to find position and existance of characters
+            If txtGuess.Text = strMainString.Chars(intThrowaway) Or txtGuess.Text.ToLower = strMainString.ToLower.Chars(intThrowaway) Then
+                'If the character of txtGuess is equal to the character of the string, or the lowercase version is equal, 
+                'Turn the bolCorrect to true, remove the specific character from lblDisplayword, insert the character, then put the character into the check guess string
+                bolCorrect = True
+                lblDisplayword.Text = lblDisplayword.Text.Remove(intThrowaway, 1) 'Removes the character to be replaced
+                lblDisplayword.Text = lblDisplayword.Text.Insert(intThrowaway, strMainString.Substring(intThrowaway, 1)) 'Inserts the character from the main string into the label
+                strGuessed = strGuessed & lblDisplayword.Text.Chars(intThrowaway)
+                SystemSounds.Asterisk.Play()
+            End If
+
+        Next
+
+        If bolCorrect = False Then
+            intWrongGuesses += 1
+            lblWrongguesses.Text = intWrongGuesses
+            SystemSounds.Hand.Play()
+            MessageBox.Show("That is not in the word!" & vbNewLine & txtGuess.Text & " is not in the word!")
+        End If
+
+        If lblDisplayword.Text = strMainString Then 'Checks if the correct guesses have gotten to the length of the string
+            My.Computer.Audio.Play(My.Resources.ding, AudioPlayMode.WaitToComplete)
+            'Win timer WIP
+            For intCounter As Integer = 1 To 3
+
+                Select Case intCounter
+                    Case 1
+                        Me.BackColor = Color.Red
+                    Case 2
+                        Me.BackColor = Color.Blue
+                    Case 3
+                        Me.BackColor = Color.Purple
+                End Select
+
+                For i As Integer = 1 To 99
+                    My.Application.DoEvents()
+                Next
+
+            Next
+                MessageBox.Show("You have fully guessed the word!" & vbNewLine & "It was " & strMainString)
+            Application.Exit()
+        End If
+
+        If intWrongGuesses = 8 Then
+            My.Computer.Audio.Play(My.Resources.remove, AudioPlayMode.WaitToComplete)
+            MessageBox.Show("You are out of guesses" & vbNewLine & "Therefor you lose!" & vbNewLine & "The Word was " & strMainString)
+            Application.Exit() 'Informs the user and then exits if they have failed
+        End If
+
+        txtGuess.Text = ""
+
+    End Sub
+
+    Private Sub btnToggle_Click(sender As Object, e As EventArgs) Handles btnToggle.Click
+
+        Static bolMusic As Boolean = False
+
+        If bolMusic Then
+            My.Computer.Audio.Stop()
+            bolMusic = False
+        Else
+            My.Computer.Audio.Play(My.Resources.Fighting_Gold_Band_Arrangement, AudioPlayMode.BackgroundLoop)
+            bolMusic = True
+        End If
+
+    End Sub
+
+    Private Sub tmrLength_Tick(sender As Object, e As EventArgs) Handles tmrLength.Tick
+
+        Static dblTime As Double
+
+        dblTime += 0.1
+        lblTimer.Text = "Time : " & Val(dblTime)
+
+    End Sub
+
+End Class
