2787279ab05a3aca6c2f29b943e546a0fb78f9fe
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-02-19 09:47:31 -0600

    added bowlingscores for EC

---
+++ Hudzik24(BowlingScoresEC)/Hudzik24(BowlingScoresEC)/frmMain.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnStatistics = New System.Windows.Forms.Button()
+        Me.lblMain = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(12, 171)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(86, 23)
+        Me.btnEnter.TabIndex = 0
+        Me.btnEnter.Text = "Enter Scores"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnStatistics
+        '
+        Me.btnStatistics.Location = New System.Drawing.Point(13, 201)
+        Me.btnStatistics.Name = "btnStatistics"
+        Me.btnStatistics.Size = New System.Drawing.Size(85, 23)
+        Me.btnStatistics.TabIndex = 1
+        Me.btnStatistics.Text = "Statistics"
+        Me.btnStatistics.UseVisualStyleBackColor = True
+        '
+        'lblMain
+        '
+        Me.lblMain.AutoSize = True
+        Me.lblMain.Location = New System.Drawing.Point(13, 32)
+        Me.lblMain.Name = "lblMain"
+        Me.lblMain.Size = New System.Drawing.Size(0, 13)
+        Me.lblMain.TabIndex = 2
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 233)
+        Me.Controls.Add(Me.lblMain)
+        Me.Controls.Add(Me.btnStatistics)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnStatistics As Button
+    Friend WithEvents lblMain As Label
+End Class
---
+++ Hudzik24(BowlingScoresEC)/Hudzik24(BowlingScoresEC)/frmMain.vb
@@ -0,0 +1,72 @@
+﻿Public Class frmMain
+
+    Dim dictScores As New Dictionary(Of Integer, Integer)
+    'First int is key, second is value
+    Dim intCounter As Integer
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Do Until 0 <> 0
+
+            Dim strCarrier As String
+            'Carries the string from the input box to be examined
+
+
+            strCarrier = InputBox("Input score, click cancel to end")
+
+            If strCarrier = "" Then
+                Exit Do
+            ElseIf strCarrier.Length >= 4 Or Val(strCarrier) > 300 Then
+                MessageBox.Show("That is an invalid score")
+                Exit Do
+            End If
+
+            intCounter += 1
+            dictScores.Add(intCounter, Val(strCarrier))
+
+        Loop
+
+    End Sub
+
+
+    Private Sub btnStatistics_Click(sender As Object, e As EventArgs) Handles btnStatistics.Click
+
+        Dim intHighscore As Integer
+        Dim intLowscore As Integer
+
+        intHighscore = dictScores.Values.Max()
+        intLowscore = dictScores.Values.Min()
+
+        'you know, I had a whole thing planned out that worked but it didn't so here is the corpse for autopsy
+        'For intCount As Integer = 1 To intCounter - 1
+        '    Dim intFirst As Integer
+        '    Dim intSecond As Integer
+
+        '    intFirst = dictScores(intCount)
+        '    intSecond = dictScores(intCount + 1)
+
+        '    If intFirst < intSecond Then
+        '        intHighscore = intSecond
+
+        '    End If
+
+        'Next
+
+        'For intCount As Integer = 1 To intCounter - 1
+        '    Dim intFirst As Integer
+        '    Dim intSecond As Integer
+
+        '    intFirst = dictScores(intCount)
+        '    intSecond = dictScores(intCount + 1)
+
+        '    If intFirst > intSecond Then
+        '        intLowscore = intFirst
+
+        '    End If
+
+        'Next
+
+    End Sub
+
+
+End Class
