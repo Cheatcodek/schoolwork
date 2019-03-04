4f273a0ef11d3a5884678d9f5a6ee419c286b519
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-03-04 09:24:54 -0600

    finished shellgame

--- Hudzik26(ShellGame)/Hudzik26(ShellGame)/frmMain.Designer.vb
+++ Hudzik26(ShellGame)/Hudzik26(ShellGame)/frmMain.Designer.vb
@@ -31,12 +31,21 @@ Partial Class frmMain
         Me.lblGamesPlayed = New System.Windows.Forms.Label()
         Me.lblGamesWon = New System.Windows.Forms.Label()
         Me.btnPlayAgain = New System.Windows.Forms.Button()
+        Me.mnuMain = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShellselect = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShellShell1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShellShell2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShellShell3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShellReset = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.picShell1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picShell2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picShell3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picPearl1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picPearl2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picPearl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.mnuMain.SuspendLayout()
         Me.SuspendLayout()
         '
         'picShell1
@@ -48,6 +57,7 @@ Partial Class frmMain
         Me.picShell1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picShell1.TabIndex = 0
         Me.picShell1.TabStop = False
+        Me.picShell1.Tag = "1"
         '
         'picShell2
         '
@@ -58,6 +68,7 @@ Partial Class frmMain
         Me.picShell2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picShell2.TabIndex = 1
         Me.picShell2.TabStop = False
+        Me.picShell2.Tag = "2"
         '
         'picShell3
         '
@@ -68,6 +79,7 @@ Partial Class frmMain
         Me.picShell3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picShell3.TabIndex = 2
         Me.picShell3.TabStop = False
+        Me.picShell3.Tag = "3"
         '
         'picPearl1
         '
@@ -98,9 +110,9 @@ Partial Class frmMain
         Me.lblGamesPlayed.AutoSize = True
         Me.lblGamesPlayed.Location = New System.Drawing.Point(12, 279)
         Me.lblGamesPlayed.Name = "lblGamesPlayed"
-        Me.lblGamesPlayed.Size = New System.Drawing.Size(85, 13)
+        Me.lblGamesPlayed.Size = New System.Drawing.Size(90, 13)
         Me.lblGamesPlayed.TabIndex = 6
-        Me.lblGamesPlayed.Text = "Game Played : 0"
+        Me.lblGamesPlayed.Text = "Games Played : 0"
         '
         'lblGamesWon
         '
@@ -120,6 +132,62 @@ Partial Class frmMain
         Me.btnPlayAgain.Text = "Play Again"
         Me.btnPlayAgain.UseVisualStyleBackColor = True
         '
+        'mnuMain
+        '
+        Me.mnuMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuShellselect})
+        Me.mnuMain.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMain.Name = "mnuMain"
+        Me.mnuMain.Size = New System.Drawing.Size(513, 24)
+        Me.mnuMain.TabIndex = 9
+        Me.mnuMain.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "File"
+        '
+        'mnuFileExit
+        '
+        Me.mnuFileExit.Name = "mnuFileExit"
+        Me.mnuFileExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuFileExit.Text = "Exit"
+        '
+        'mnuShellselect
+        '
+        Me.mnuShellselect.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuShellShell1, Me.mnuShellShell2, Me.mnuShellShell3, Me.mnuShellReset})
+        Me.mnuShellselect.Name = "mnuShellselect"
+        Me.mnuShellselect.Size = New System.Drawing.Size(78, 20)
+        Me.mnuShellselect.Text = "Shell Select"
+        '
+        'mnuShellShell1
+        '
+        Me.mnuShellShell1.Name = "mnuShellShell1"
+        Me.mnuShellShell1.Size = New System.Drawing.Size(180, 22)
+        Me.mnuShellShell1.Tag = "1"
+        Me.mnuShellShell1.Text = "Shell 1"
+        '
+        'mnuShellShell2
+        '
+        Me.mnuShellShell2.Name = "mnuShellShell2"
+        Me.mnuShellShell2.Size = New System.Drawing.Size(180, 22)
+        Me.mnuShellShell2.Tag = "2"
+        Me.mnuShellShell2.Text = "Shell 2"
+        '
+        'mnuShellShell3
+        '
+        Me.mnuShellShell3.Name = "mnuShellShell3"
+        Me.mnuShellShell3.Size = New System.Drawing.Size(180, 22)
+        Me.mnuShellShell3.Tag = "3"
+        Me.mnuShellShell3.Text = "Shell 3"
+        '
+        'mnuShellReset
+        '
+        Me.mnuShellReset.Name = "mnuShellReset"
+        Me.mnuShellReset.Size = New System.Drawing.Size(180, 22)
+        Me.mnuShellReset.Text = "Reset Game"
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -134,6 +202,8 @@ Partial Class frmMain
         Me.Controls.Add(Me.picShell3)
         Me.Controls.Add(Me.picShell2)
         Me.Controls.Add(Me.picShell1)
+        Me.Controls.Add(Me.mnuMain)
+        Me.MainMenuStrip = Me.mnuMain
         Me.Name = "frmMain"
         Me.Text = "Shell Game"
         CType(Me.picShell1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -142,6 +212,8 @@ Partial Class frmMain
         CType(Me.picPearl1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picPearl2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picPearl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.mnuMain.ResumeLayout(False)
+        Me.mnuMain.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -156,4 +228,12 @@ Partial Class frmMain
     Friend WithEvents lblGamesPlayed As Label
     Friend WithEvents lblGamesWon As Label
     Friend WithEvents btnPlayAgain As Button
+    Friend WithEvents mnuMain As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuFileExit As ToolStripMenuItem
+    Friend WithEvents mnuShellselect As ToolStripMenuItem
+    Friend WithEvents mnuShellShell1 As ToolStripMenuItem
+    Friend WithEvents mnuShellShell2 As ToolStripMenuItem
+    Friend WithEvents mnuShellShell3 As ToolStripMenuItem
+    Friend WithEvents mnuShellReset As ToolStripMenuItem
 End Class
--- Hudzik26(ShellGame)/Hudzik26(ShellGame)/frmMain.vb
+++ Hudzik26(ShellGame)/Hudzik26(ShellGame)/frmMain.vb
@@ -2,46 +2,126 @@
 
     Dim intGamesPlayed As Integer = 0
     Dim intGamesWon As Integer = 0
+    Dim bolGamePlayed = False
 
+    'Creative addition, Resets the game when the user wishes to
     Private Sub btnPlayAgain_Click(sender As Object, e As EventArgs) Handles btnPlayAgain.Click, MyBase.Load
 
-        'Dim objRandom As New Random
+        picPearl1.Image = Nothing
+        picPearl2.Image = Nothing
+        picPearl3.Image = Nothing
 
-        'Select Case objRandom.Next(1, 4)
-        '    Case 1
-        '        picShell1.Tag = "1"
-        '        picShell2.Tag = "0"
-        '        picShell3.Tag = "0"
-        '    Case 2
-        '        picShell1.Tag = "0"
-        '        picShell2.Tag = "1"
-        '        picShell3.Tag = "0"
-        '    Case 3
-        '        picShell1.Tag = "0"
-        '        picShell2.Tag = "0"
-        '        picShell3.Tag = "1"
-        'End Select
-
-
+        bolGamePlayed = False
 
     End Sub
 
     Private Sub picShell_Click(sender As Object, e As EventArgs) Handles picShell1.Click, picShell2.Click, picShell3.Click
 
         Dim objRandom As New Random
-
         Dim picSelectedShell As PictureBox = sender
+        Dim intWinner As Integer = objRandom.Next(1, 4)
 
+        'Only let this pass if the player has reset the game
+        If bolGamePlayed = True Then
+            MessageBox.Show("Restart the game!!" & vbNewLine & "Click Play Again!!")
+            Exit Sub
+        Else
+            bolGamePlayed = True
+        End If
+        'If the user has selected the correct answer, say that they have won. Otherwise say they have failed. In either case, display the correct pearl
+        If picSelectedShell.Tag = intWinner Then
 
+            Select Case picSelectedShell.Tag
+                Case Val(1)
+                    picPearl1.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+                Case Val(2)
+                    picPearl2.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+                Case Val(3)
+                    picPearl3.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+            End Select
 
-        If Val(picSelectedShell.Tag) = 1 Then
-
-            MessageBox.Show("Win")
-        ElseIf Val(picSelectedShell.Tag) = 0 Then
-
-            MessageBox.Show("Loss")
+        Else
+            Select Case intWinner
+                Case Val(1)
+                    picPearl1.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+                Case Val(2)
+                    picPearl2.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+                Case Val(3)
+                    picPearl3.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+            End Select
         End If
 
+        'Counters for games played and displaying games played and won
+        intGamesPlayed += 1
+        lblGamesPlayed.Text = "Games Played : " & intGamesPlayed
+        lblGamesWon.Text = "Games Won : " & intGamesWon
+
+    End Sub
+
+    Private Sub mnuShell_Click(sender As Object, e As EventArgs) Handles mnuShellShell1.Click, mnuShellShell2.Click, mnuShellShell3.Click
+
+        Dim objRandom As New Random
+        Dim mnuSelectedShell As ToolStripMenuItem = sender
+        Dim intWinner As Integer = objRandom.Next(1, 4)
+
+        'See comments on the picturebox handler for details
+        If bolGamePlayed = True Then
+            MessageBox.Show("Restart the game!!" & vbNewLine & "Click Play Again!!")
+            Exit Sub
+        Else
+            bolGamePlayed = True
+        End If
+
+        If mnuSelectedShell.Tag = intWinner Then
+
+            Select Case mnuSelectedShell.Tag
+                Case Val(1)
+                    picPearl1.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+                Case Val(2)
+                    picPearl2.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+                Case Val(3)
+                    picPearl3.Image = My.Resources.Pearl
+                    MessageBox.Show("You are Winner!!")
+                    intGamesWon += 1
+            End Select
+
+        Else
+            Select Case intWinner
+                Case Val(1)
+                    picPearl1.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+                Case Val(2)
+                    picPearl2.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+                Case Val(3)
+                    picPearl3.Image = My.Resources.Pearl
+                    MessageBox.Show("You have Fail!!")
+            End Select
+        End If
+
+
+        intGamesPlayed += 1
+        lblGamesPlayed.Text = "Games Played : " & intGamesPlayed
+        lblGamesWon.Text = "Games Won : " & intGamesWon
+
+    End Sub
+
+
+    Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+        Application.Exit()
     End Sub
 
 
