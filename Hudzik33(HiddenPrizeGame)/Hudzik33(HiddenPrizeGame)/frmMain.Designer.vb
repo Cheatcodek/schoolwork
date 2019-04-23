82250c5f7f76459dc10ebfdd4fc4c233fe5c1746
Cheatcodek <43485435+Cheatcodek@users.noreply.github.com>
2019-04-23 09:47:24 -0500

    Add files via upload

---
+++ Hudzik33(HiddenPrizeGame)/Hudzik33(HiddenPrizeGame)/frmMain.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnHidePrizes = New System.Windows.Forms.Button()
+        Me.lblBoxesLeft = New System.Windows.Forms.Label()
+        Me.radNormalMode = New System.Windows.Forms.RadioButton()
+        Me.radEasyMode = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'btnHidePrizes
+        '
+        Me.btnHidePrizes.Location = New System.Drawing.Point(111, 259)
+        Me.btnHidePrizes.Name = "btnHidePrizes"
+        Me.btnHidePrizes.Size = New System.Drawing.Size(75, 56)
+        Me.btnHidePrizes.TabIndex = 0
+        Me.btnHidePrizes.Text = "HIDE " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "THOSE" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PRIZES !!"
+        Me.btnHidePrizes.UseVisualStyleBackColor = True
+        '
+        'lblBoxesLeft
+        '
+        Me.lblBoxesLeft.AutoSize = True
+        Me.lblBoxesLeft.Location = New System.Drawing.Point(13, 259)
+        Me.lblBoxesLeft.Name = "lblBoxesLeft"
+        Me.lblBoxesLeft.Size = New System.Drawing.Size(74, 13)
+        Me.lblBoxesLeft.TabIndex = 1
+        Me.lblBoxesLeft.Text = "Boxes left : 10"
+        Me.lblBoxesLeft.Visible = False
+        '
+        'radNormalMode
+        '
+        Me.radNormalMode.AutoSize = True
+        Me.radNormalMode.Checked = True
+        Me.radNormalMode.Location = New System.Drawing.Point(193, 257)
+        Me.radNormalMode.Name = "radNormalMode"
+        Me.radNormalMode.Size = New System.Drawing.Size(88, 17)
+        Me.radNormalMode.TabIndex = 2
+        Me.radNormalMode.TabStop = True
+        Me.radNormalMode.Text = "Normal Mode"
+        Me.radNormalMode.UseVisualStyleBackColor = True
+        '
+        'radEasyMode
+        '
+        Me.radEasyMode.AutoSize = True
+        Me.radEasyMode.Location = New System.Drawing.Point(193, 281)
+        Me.radEasyMode.Name = "radEasyMode"
+        Me.radEasyMode.Size = New System.Drawing.Size(78, 17)
+        Me.radEasyMode.TabIndex = 3
+        Me.radEasyMode.Text = "Easy Mode"
+        Me.radEasyMode.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 327)
+        Me.Controls.Add(Me.radEasyMode)
+        Me.Controls.Add(Me.radNormalMode)
+        Me.Controls.Add(Me.lblBoxesLeft)
+        Me.Controls.Add(Me.btnHidePrizes)
+        Me.Name = "frmMain"
+        Me.Text = "Hidden Prizes!!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnHidePrizes As Button
+    Friend WithEvents lblBoxesLeft As Label
+    Friend WithEvents radNormalMode As RadioButton
+    Friend WithEvents radEasyMode As RadioButton
+End Class
---
+++ Hudzik33(HiddenPrizeGame)/Hudzik33(HiddenPrizeGame)/frmMain.vb
@@ -0,0 +1,118 @@
+﻿Public Class frmMain
+
+    Dim btnHiddenBoxes(4, 4) As Button
+
+    Dim intButtonsGiven As Integer
+    Dim strPrize As String
+
+
+    Sub CreateButtons(ByVal intButtonAmount As Integer)
+
+
+        'For loops to create buttons
+        For i As Integer = 0 To intButtonAmount
+
+            For j As Integer = 0 To intButtonAmount
+
+                btnHiddenBoxes(i, j) = New Button
+                btnHiddenBoxes(i, j).Text = (i + 1) & ", " & (j + 1)
+                btnHiddenBoxes(i, j).Size = New Size(50, 30)
+                btnHiddenBoxes(i, j).Location = New Point(i * 56, j * 32)
+                btnHiddenBoxes(i, j).Enabled = False
+                Controls.Add(btnHiddenBoxes(i, j))
+
+                AddHandler btnHiddenBoxes(i, j).Click, AddressOf PrizeRevealer
+
+            Next
+
+        Next
+
+    End Sub
+
+    Private Sub btnHidePrizes_Click(sender As Object, e As EventArgs) Handles btnHidePrizes.Click
+
+        If radNormalMode.Checked = True Then
+            intButtonsGiven = 4
+        Else
+            intButtonsGiven = 2
+            ReDim btnHiddenBoxes(2, 2)
+        End If
+
+        CreateButtons(intButtonsGiven)
+        HidePrizes(intButtonsGiven)
+        btnHidePrizes.Enabled = False
+    End Sub
+
+
+    Sub HidePrizes(ByVal intButtonAmount As Integer)
+        Static objRandom As New Random
+
+        For i As Integer = 0 To intButtonAmount
+            For j As Integer = 0 To intButtonAmount
+                btnHiddenBoxes(i, j).Enabled = True
+            Next
+        Next
+
+        'Gives the values of "Comp" and "uter" randomly
+        btnHiddenBoxes(objRandom.Next(1, intButtonAmount), objRandom.Next(1, intButtonAmount)).Tag = "Comp"
+        btnHiddenBoxes(objRandom.Next(1, intButtonAmount), objRandom.Next(1, intButtonAmount)).Tag = "uter"
+
+
+
+        Dim strCheckString As String = ""
+
+        For i As Integer = 0 To intButtonAmount
+            For j As Integer = 0 To intButtonAmount
+                strCheckString = strCheckString & btnHiddenBoxes(i, j).Tag
+            Next
+        Next
+
+        'If the tags are not on the buttons, redo
+        If (strCheckString = "Computer") Or (strCheckString = "uterComp") Then
+        Else
+            Call HidePrizes(intButtonsGiven)
+        End If
+
+        lblBoxesLeft.Visible = True
+
+
+    End Sub
+
+
+    Private Sub PrizeRevealer(ByVal sender As Object, ByVal e As System.EventArgs)
+
+        Dim btnScannedButton As Button = sender
+        Static strCheckString As String = ""
+        Static intBoxesLeft As Integer = 10
+
+        btnScannedButton.Enabled = False
+
+        'These two ifs check if the user got comp or user then adds it to the checkstring
+        If sender.Tag = "Comp" Then
+            MessageBox.Show("You got Comp !!")
+            strCheckString = strCheckString & "Comp"
+        End If
+
+        If sender.Tag = "uter" Then
+            MessageBox.Show("You got uter !!")
+            strCheckString = strCheckString & "uter"
+        End If
+
+        'Victory condition
+        If (strCheckString = "Computer") Or (strCheckString = "uterComp") Then
+            MessageBox.Show("You got Computer!!" & vbNewLine & "You won the computer")
+            Application.Exit()
+        End If
+
+        intBoxesLeft -= 1
+        lblBoxesLeft.Text = "Boxes left : " & intBoxesLeft
+
+        If intBoxesLeft <= 0 Then
+            MessageBox.Show("You've ran out of uses!!" & vbNewLine & "Goodbye")
+            Application.Exit()
+        End If
+
+
+    End Sub
+
+End Class
